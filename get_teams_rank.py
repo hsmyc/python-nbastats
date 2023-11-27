3dd698c90e76f2f182ba15315431ddd9c999957b
leonidyuce <hllsmtyc@gmail.com>
2023-11-27 17:27:44 +0100

    edited

--- get_teams_rank.py
+++ get_teams_rank.py
@@ -17,14 +17,6 @@ def get_teams_rank():
     result_sets = response_json['resultSets']
     headers = result_sets[0]['headers']
     rows = result_sets[0]['rowSet']
-    all_stats = []
-    for row in rows:
-        stats = dict(zip(headers, row))
-        all_stats.append(stats)
-    with open('teamrank.json', 'w', encoding='utf-8') as outfile:
-        json.dump(all_stats, outfile, sort_keys=True, indent=4)
-    with open('teamrank.json', 'r', encoding='utf-8') as f:
-        data = json.load(f)
     with open('teamrank.csv', 'w', newline='', encoding='utf-8') as csvfile:
         fieldnames = [
             "TEAM_ID",
@@ -33,44 +25,12 @@ def get_teams_rank():
             "W",
             "L",
             "W_PCT",
-            "MIN",
-            "OPP_FGM",
-            "OPP_FGA",
-            "OPP_FG_PCT",
-            "OPP_FG3M",
-            "OPP_FG3A",
-            "OPP_FG3_PCT",
-            "OPP_FTM",
-            "OPP_FTA",
-            "OPP_FT_PCT",
-            "OPP_OREB",
-            "OPP_DREB",
-            "OPP_REB",
-            "OPP_AST",
-            "OPP_TOV",
-            "OPP_STL",
-            "OPP_BLK",
-            "OPP_BLKA",
-            "OPP_PF",
-            "OPP_PFD",
-            "OPP_PTS",
             "PLUS_MINUS",
-            "GP_RANK",
-            "W_RANK",
-            "L_RANK",
             "W_PCT_RANK",
-            "MIN_RANK",
-            "OPP_FGM_RANK",
-            "OPP_FGA_RANK",
             "OPP_FG_PCT_RANK",
             "OPP_FG3M_RANK",
-            "OPP_FG3A_RANK",
             "OPP_FG3_PCT_RANK",
-            "OPP_FTM_RANK",
-            "OPP_FTA_RANK",
             "OPP_FT_PCT_RANK",
-            "OPP_OREB_RANK",
-            "OPP_DREB_RANK",
             "OPP_REB_RANK",
             "OPP_AST_RANK",
             "OPP_TOV_RANK",
@@ -84,8 +44,31 @@ def get_teams_rank():
         ]
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
-        for row in data:
-            writer.writerow(row)
+        for row in rows:
+            writer.writerow({
+                "TEAM_ID": row[headers.index('TEAM_ID')],
+                "TEAM_NAME": row[headers.index('TEAM_NAME')],
+                "GP": row[headers.index('GP')],
+                "W": row[headers.index('W')],
+                "L": row[headers.index('L')],
+                "W_PCT": row[headers.index('W_PCT')],
+                "PLUS_MINUS": row[headers.index('PLUS_MINUS')],
+                "W_PCT_RANK": row[headers.index('W_PCT_RANK')],
+                "OPP_FG_PCT_RANK": row[headers.index('OPP_FG_PCT_RANK')],
+                "OPP_FG3M_RANK": row[headers.index('OPP_FG3M_RANK')],
+                "OPP_FG3_PCT_RANK": row[headers.index('OPP_FG3_PCT_RANK')],
+                "OPP_FT_PCT_RANK": row[headers.index('OPP_FT_PCT_RANK')],
+                "OPP_REB_RANK": row[headers.index('OPP_REB_RANK')],
+                "OPP_AST_RANK": row[headers.index('OPP_AST_RANK')],
+                "OPP_TOV_RANK": row[headers.index('OPP_TOV_RANK')],
+                "OPP_STL_RANK": row[headers.index('OPP_STL_RANK')],
+                "OPP_BLK_RANK": row[headers.index('OPP_BLK_RANK')],
+                "OPP_BLKA_RANK": row[headers.index('OPP_BLKA_RANK')],
+                "OPP_PF_RANK": row[headers.index('OPP_PF_RANK')],
+                "OPP_PFD_RANK": row[headers.index('OPP_PFD_RANK')],
+                "OPP_PTS_RANK": row[headers.index('OPP_PTS_RANK')],
+                "PLUS_MINUS_RANK": row[headers.index('PLUS_MINUS_RANK')]
+            })
 
     print('Done!')
 
